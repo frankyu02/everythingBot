7b2626df7973eff9f209cbca09073d85e16a5ce1
frankyu02 <frankyu02@yahoo.ca>
2022-02-13 21:28:33 +0000

    first commit

---
+++ bot.js
@@ -0,0 +1,29 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES
+  ] 
+  });
+const token = process.env['token']
+client.on('ready', () => {
+  console.log(`successfully logged in`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+  else if (interaction.commandName === 'date'){
+    await interaction.reply('date goes here');
+  }
+});
+
+client.on('messageCreate', (message) => {
+  if(message.content === "!date"){
+    message.channel.send('test complete');
+  }
+});
+
+client.login(token);
