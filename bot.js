8da86ed13f746562391db9c4ad9c649507d86cc7
Frank Yu <58200308+frankyu02@users.noreply.github.com>
2022-02-13 22:00:37 +0000

    date command added

--- bot.js
+++ bot.js
@@ -8,21 +8,40 @@ const token = process.env['token']
 client.on('ready', () => {
   console.log(`successfully logged in`);
 });
-
+function getDatePrefix(date){
+  if(date % 10 === 1){
+    return "st ";
+  }
+  else if(date % 10 === 2 ){
+    return "nd ";
+  }
+  else if(date % 10 === 3 && date !== 13){
+    return "rd ";
+  } else{
+    return "th ";
+  }
+}
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const date = new Date()
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
   if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
   }
-  else if (interaction.commandName === 'date'){
-    await interaction.reply('date goes here');
+  if (interaction.commandName === 'date'){
+    await interaction.reply("Today's date: " + months[date.getMonth()] + ' ' + 
+    date.getDate() + getDatePrefix(date.getDate()) + date.getFullYear() + '.');
+  }
+  if (interaction.commandName === 'version'){
+    await interaction.reply('current version 1.0.0 BETA');
   }
 });
 
 client.on('messageCreate', (message) => {
   if(message.content === "!date"){
-    message.channel.send('test complete');
+    message.channel.send("Today's date: " + months[date.getMonth()] + ' ' + 
+    date.getDate() + getDatePrefix(date.getDate()) + date.getFullYear() + '.');
   }
   else if(message.content === "!version"){
     message.channel.send('current version: 1.0.0 BETA');
